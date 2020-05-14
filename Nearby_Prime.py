c37158cdebc24fe980d0d93f4a1eb3db37b65c8f
deepakkmr896 <deepakkmr896@gmail.com>
2020-05-14 15:09:12 +0530

    - Added the comments

--- Nearby_Prime.py
+++ Nearby_Prime.py
@@ -1,17 +1,21 @@
-val = int(input("Input a value\n"))
+input_val = int(input("Input a value\n")) # Get the input from the entered number
 nearestPrimeNum = [];
+
+# Define a function to check the prime number
 def isPrime(num):
     isPrime = True
-    for i in range(2, (num // 2)):
+    for i in range(2, (num // 2) + 1):
         if(num % i == 0):
             isPrime = False
     return isPrime
 
+# Assuming 10 as the maximum gap between the successive prime number
 for i in range(1, 11):
-    precVal = val - i;
-    forwVal = val + i;
+    precVal = input_val - i;
+    forwVal = input_val + i;
     
-    if (precVal > 0):
+    # Check both the preceding and succeeding number for the prime and capture whichever is found first or both (e.g. 3 and 5 for 7)
+    if (precVal > 1):
         if (isPrime(precVal) == True):
             nearestPrimeNum.append(precVal)
     if (isPrime(forwVal) == True):
